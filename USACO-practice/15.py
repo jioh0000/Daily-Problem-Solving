15124cffaf15887c3c5d826023d56a54825739d1
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-08-18 08:24:48 +0900

    Update 15.py

--- USACO-practice/15.py
+++ USACO-practice/15.py
@@ -1,5 +1,16 @@
-N, K = map(int,input().split())
-arr = [list(map(int,input().split())) for _ in range(N)]
+N = int(input())
+arr = []
+for i in range(N):
+    arr.append([list(map(int, input().split())) for _ in range(3)])
+
+for i in range(N):
+    answer = -1
+    diffN = arr[i][0][1] - arr[i][1][3]
+    diffS = arr[i][1][1]
+    diffE = arr[i][0][0] - arr[i][1][2]
+    diffW = arr[i][1][0]
+
+    if max(diffN+diffE, diffE+diffS, diffS+diffW, diffW+diffN) == diffN+diffE:
+        x1, x2, y1, y2 = arr[i][0][0]
 
 print(arr)
-
