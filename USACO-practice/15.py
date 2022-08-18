ef0e65293e1fcbfb34744c766b075d004b58d25d
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-08-18 09:32:58 +0900

    Update 15.py

--- USACO-practice/15.py
+++ USACO-practice/15.py
@@ -1,3 +1,7 @@
+"""
+[[[8, 5], [2, 1, 7, 4], [4, 2]], [[5, 4], [2, 2, 5, 4], [3, 3]], [[1, 8], [0, 3, 1, 6], [1, 5]]]
+"""
+x1,y1,x2,y2,h,l,movableX,movableY = 0,0,0,0,0,0,0,0
 N = int(input())
 arr = []
 for i in range(N):
@@ -9,8 +13,38 @@ for i in range(N):
     diffS = arr[i][1][1]
     diffE = arr[i][0][0] - arr[i][1][2]
     diffW = arr[i][1][0]
+    a = max(diffN+diffE, diffE+diffS, diffS+diffW, diffW+diffN)
 
-    if max(diffN+diffE, diffE+diffS, diffS+diffW, diffW+diffN) == diffN+diffE:
-        x1, x2, y1, y2 = arr[i][0][0]
-
-print(arr)
+    if a == diffN + diffE:
+        x1, y1, x2, y2 = arr[i][0][0]-arr[i][2][0], arr[i][0][1]-arr[i][2][1], arr[i][0][0], arr[i][0][1]
+        h = min(arr[i][1][3], y2) - max(arr[i][1][1], y1)
+        l = min(arr[i][1][2], x2) - max(arr[i][1][0], x1)
+        movableX = diffW
+        movableY = diffS
+    elif a == diffE + diffS:
+        x1, y1, x2, y2 = arr[i][0][0]-arr[i][2][0], 0, arr[i][0][0], arr[i][2][1]
+        h = min(arr[i][1][3], y2) - max(arr[i][1][1], y1)
+        l = min(arr[i][1][2], x2) - max(arr[i][1][0], x1)
+        movableX = diffW
+        movableY = diffN
+    elif a == diffS + diffW:
+        x1, y1, x2, y2 = 0, 0, arr[i][2][0], arr[i][2][1]
+        h = min(arr[i][1][3], y2) - max(arr[i][1][1], y1)
+        l = min(arr[i][1][2], x2) - max(arr[i][1][0], x1)
+        movableX = diffE
+        movableY = diffN
+    elif a == diffW + diffN:
+        x1, y1, x2, y2 = 0, arr[i][0][1]-arr[i][2][1], arr[i][2][0], arr[i][0][1]
+        h = min(arr[i][1][3], y2) - max(arr[i][1][1], y1)
+        l = min(arr[i][1][2], x2) - max(arr[i][1][0], x1)
+        movableX = diffE
+        movableY = diffS
+    
+    if h > movableY and l > movableX:
+        print(answer)
+    elif h <= movableY and l > movableX:
+        print(h)
+    elif h > movableY and l <= movableX:
+        print(l)
+    else:
+        print(min(h,l))
