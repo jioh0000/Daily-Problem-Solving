aa41db87c10738d7b659672acbaf2b3e68690861
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-08-31 23:30:37 +0900

    Update 16.py

--- USACO-practice/16.py
+++ USACO-practice/16.py
@@ -1,7 +1,29 @@
 N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-
+L = []
+G = []
+count = 0
 for i in range(N):
-    for j in range(len(arr[i])):
-        print(arr[i][j], end=" ")
-    print()
+    a, b = map(str, input().split())
+    if a == 'G':
+        G.append(int(b))
+    elif a == 'L':
+        L.append(int(b))
+
+L.sort()
+G.sort()
+
+
+while(G[-1] > L[0]):
+    if len(G) == 1 or len(L) == 1:
+        count += 1
+        break
+    c = G[-1] - G[-2]
+    d = L[1] - L[0]
+
+    if c>d:
+        del G[-1]
+    elif c<=d:
+        del L[0]
+    count += 1
+
+print(count)
