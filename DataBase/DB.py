23c3c23bd479a469858b162975f5c8e3e3330dc3
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-09-27 23:31:54 +0900

    Update DB.py

--- DataBase/DB.py
+++ DataBase/DB.py
@@ -2,9 +2,6 @@ import sqlite3
 
 con = sqlite3.connect("test.db")
 cur = con.cursor()
-#cur.execute("CREATE TABLE PhoneBook(Name text, PhoneNum text);")
-#cur.execute("INSERT INTO PhoneBook VALUES('Someone', '010-2777-1209');")
-
-
-cur.execute("INSERT INTO PhoneBook VALUES('Someone', '010-2777-1209');") 
-
+cur.execute("CREATE TABLE PhoneBook(Name text, PhoneNum text);")
+cur.execute("INSERT INTO PhoneBook VALUES('Jioh Park', '010-2777-1209');") 
+cur.execute("INSERT INTO PhoneBook VALUES('Lim San', '010 9384 3847);")
