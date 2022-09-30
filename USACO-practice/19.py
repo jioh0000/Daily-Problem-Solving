d5e24bb69524ba413f09743c3a5a73b9721b7c0a
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-09-30 09:13:50 +0900

    Update 19.py

--- USACO-practice/19.py
+++ USACO-practice/19.py
@@ -5,3 +5,6 @@ answer = 0
 for i in range(0, len(A) - 1):
     if i == A.index(A[i]):
         storage = []
+        index = i + 1
+        while(A[index] != A[i]):
+            storage.append(A[index])
