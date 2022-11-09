057fd2b13860edc622c12b6b55dcedb92935594c
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-11-09 09:50:21 +0900

    Update K5.py

--- KCSL/K5.py
+++ KCSL/K5.py
@@ -1,1 +1,15 @@
 N, K = map(int, input().split(', '))
+sum = 0
+
+
+for i in range(0, len(str(N)) - K + 1):
+    tenth = 10**(K-1)
+    count = 0
+    temp = 0
+    for j in range(K):
+        temp += int((str(N))[i+count])*tenth
+        count += 1
+        tenth /= 10
+    sum += temp
+
+print(int(sum))
