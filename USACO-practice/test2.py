cff6e494dfe227a042f51fb4c2f4ae5426304246
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-12-20 17:01:27 +0900

    Create test2.py

---
+++ USACO-practice/test2.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for i in range(T):
+    N, M = map(int, input().split(" "))
+    for _ in range(M):
+        L, K = map(int, input().split(" "))
+
+print("OK")
+print("OK")
+print("LIE")
+print("LIE")
+
