b8259901809ea37f771b267183617681ccd126ea
jioh0000 <101187812+jioh0000@users.noreply.github.com>
2022-12-26 16:06:33 +0900

    Update 23.py

--- USACO-practice/23.py
+++ USACO-practice/23.py
@@ -1,25 +1,14 @@
-N, Q = map(int, input().split(" "))
-arr = [0]
+import sys
+N, Q = map(int, sys.stdin.readline().split(" "))
 arr1 = [0]# arr1[i] : 1~i까지 1의 개수
 arr2 = [0]
 arr3 = [0]
 for i in range(N):
-    a = int(input())
-    arr.append(a)
-    
-    if a == 1:
-        arr1.append(arr1[i] + 1)
-        arr2.append(arr2[i] + 0)
-        arr3.append(arr3[i] + 0)
-    elif a == 2:
-        arr1.append(arr1[i] + 0)
-        arr2.append(arr2[i] + 1)
-        arr3.append(arr3[i] + 0)
-    elif a == 3:
-        arr1.append(arr1[i] + 0)
-        arr2.append(arr2[i] + 0)
-        arr3.append(arr3[i] + 1)
+    a = int(sys.stdin.readline())
+    arr1.append(arr1[-1] + a==1)
+    arr2.append(arr2[-1] + a==2)
+    arr3.append(arr3[-1] + a==3)
 
 for _ in range(Q):
-    a, b = map(int, input().split(" "))
+    a, b = map(int, sys.stdin.readline().split(" "))
     print(arr1[b] - arr1[a-1], arr2[b] - arr2[a-1], arr3[b] - arr3[a-1])
