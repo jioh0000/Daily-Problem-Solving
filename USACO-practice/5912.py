e2abc450957d83cb727bb4e83e9ddf00b26fcd4a
jioh0000 <jopark24@kis.ac>
2023-01-04 23:10:13 +0900

    Update 5912.py

--- USACO-practice/5912.py
+++ USACO-practice/5912.py
@@ -2,12 +2,15 @@ import sys
 
 N, K = map(int, sys.stdin.readline().split(" "))
 # Find sequence p such that p has prefix sum sequence which is identical to arr
-arr = [0]*N
+arr = [0]*(N + 10)
+brr = [0]*(N + 10)
 
 for _ in range(K):
     a, b = map(int, sys.stdin.readline().split(" "))
-    for i in range(a-1, b):
-        arr[i] += 1
+    brr[a] += 1
+    brr[b+1] -= 1
 
-arr.sort()
-print(arr[(N-1)/2])
+for i in range(1,N+1):
+    arr[i] = arr[i - 1] + brr[i]
+
+print(sorted(arr[0:N])[(N - 1)//2])
