1ed7d488354053b8098e08718fab9c4e72173b4a
jioh0000 <jopark24@kis.ac>
2023-01-15 16:57:33 +0900

    Update 25.py

--- USACO-practice/25.py
+++ USACO-practice/25.py
@@ -16,3 +16,17 @@ for _ in range(N):
     arr.append(Pair(a,b))
 
 arr.sort()
+
+maxY = 0
+minY = 1000000001
+for i in range(N):
+    if arr[i].y > maxY:
+        maxY = arr[i].y
+    if arr[i].y < minY:
+        minY = arr[i].y
+
+onefence = (arr[N-1].x - arr[0].x) * (maxY - minY)
+
+sum1 = [0]
+for i in range(N):
+    sum1.append()
