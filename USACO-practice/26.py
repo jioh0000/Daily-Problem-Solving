e2b6392c524767da5f89b2e2db64f1d06216eaa3
jioh0000 <jopark24@kis.ac>
2023-01-24 18:19:07 +0900

    Update 26.py

--- USACO-practice/26.py
+++ USACO-practice/26.py
@@ -1,6 +1,16 @@
 import sys
+'''
+6 15
+4 3 8 4 7 3
+'''
 
-N, X = map(int, sys.stdin.readline())
-arr = []
-for _ in range(X):
-    arr.append(int(sys.stdin.readline()))
+N, X = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+arr.sort()
+
+cnt = 0
+while X >= 0 and cnt != N:
+    X -= arr[cnt]
+    cnt += 1
+
+print(cnt - 1)
